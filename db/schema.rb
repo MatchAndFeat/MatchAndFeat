2cfe38b1fd2087cf197c32294eecccba93c7000a
kalu6334 <martin.lucas76334@gmail.com>
2021-12-08 10:41:45 +0100

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 2021_12_08_080459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "feats", force: :cascade do |t|
     t.string "title"
     t.bigint "user_id"
@@ -33,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_12_08_080459) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
-  end 
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -46,7 +45,6 @@ ActiveRecord::Schema.define(version: 2021_12_08_080459) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
