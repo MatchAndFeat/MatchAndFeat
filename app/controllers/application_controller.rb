e880543d1f409c1ddf522c262ab0976aae635906
fleopaulD <flo_thp@netc.pl>
2021-12-08 14:34:00 +0100

    add user not logged in restrictions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "Merci de vous connecter pour accéder à cette page."
+      redirect_to new_user_session_path
+    end
+  end
 end
