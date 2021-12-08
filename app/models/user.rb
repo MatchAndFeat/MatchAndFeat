3e9237bf77bc0817cb80e7605c408839fc17e21c
KevinPiriou <kevinpiriou@yahoo.fr>
2021-12-08 11:44:37 +0100

    add_mailer_v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_create :welcome_send
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :user_name, presence: true
+
+
+
+  private
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+  
 end
