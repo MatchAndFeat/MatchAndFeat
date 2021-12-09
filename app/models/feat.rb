73bab79373779a1c32e376db98014a19c857e6cb
fleopaulD <flo_thp@netc.pl>
2021-12-10 00:58:48 +0100

    active storage validations implemented

--- app/models/feat.rb
+++ app/models/feat.rb
@@ -1,6 +1,13 @@
 class Feat < ApplicationRecord
-    belongs_to :project
-    belongs_to :user
-    has_many_attached :attachements
-    validates :title, presence: true, length: { in: 5..150 }  
+  belongs_to :project
+  belongs_to :user
+  has_many_attached :attachements
+  
+  validates :title, presence: true, length: { in: 5..150 }  
+  validates :attachements, 
+  size: { less_than: 20.megabytes },
+  content_type: ['image/png', 'image/jpg', 'image/jpeg',
+                  'audio/x-wav', 'audio/mpeg',
+                  'video/mpeg',
+                  'text/plain']
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,18 @@
 class Project < ApplicationRecord
-    has_many :feats
-    has_many_attached :attachements
-    has_one_attached :picture
-    belongs_to :user 
-    validates :title, presence: true, length: { in: 5..150 }
-    validates :description, presence: true, length: { in: 5..3000 }
+  has_many :feats
+  has_many_attached :attachements
+  has_one_attached :picture
+  belongs_to :user 
+  
+  validates :title, presence: true, length: { in: 5..150 }
+  validates :description, presence: true, length: { in: 5..3000 }
+  validates :picture, 
+    content_type: ['image/png', 'image/jpg', 'image/jpeg'],
+    size: { less_than: 3.megabytes }
+  validates :attachements, 
+    size: { less_than: 20.megabytes },
+    content_type: ['image/png', 'image/jpg', 'image/jpeg',
+                   'audio/x-wav', 'audio/mpeg',
+                   'video/mpeg',
+                   'text/plain']
 end
