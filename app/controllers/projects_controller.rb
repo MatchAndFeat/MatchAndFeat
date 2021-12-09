01bfaafbaa8aa632591b171d877197cfd61ec8e9
kalu6334 <martin.lucas76334@gmail.com>
2021-12-09 14:56:15 +0100

    access show no loggin

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :require_login, except: [:index, :show]
+  before_action :require_login, except: [:index] 
   before_action :set_project, except: [:index, :new]
   before_action :ownership_verification, only: [:edit, :update, :destroy]
 
