0d99526421875102f9f33b1b4858c973b0065deb
KevinPiriou <kevinpiriou@yahoo.fr>
2021-12-14 17:33:33 +0100

    add_validates_skills

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,8 @@
 class Skill < ApplicationRecord
   has_and_belongs_to_many :users
   has_and_belongs_to_many :projects
-  validates :name, presence: true
+  validates :name,
+    presence: true,
+    length: { in: 3..25 },
+    uniqueness: true
 end
