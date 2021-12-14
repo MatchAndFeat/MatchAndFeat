19d3980a7ce699886e3cf90fcef442b2a67e2102
fleopaulD <flo_thp@netc.pl>
2021-12-14 23:08:23 +0100

    implement feats likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,20 +3,19 @@ class LikesController < ApplicationController
 
   def update
     case params[:likeable]
-    when "project" then target = Project.find(params[:id])
-    when "feat"    then target = Feat.find(params[:id])
+    when "project" then @target = Project.find(params[:id])
+    when "feat"    then @target = Feat.find(params[:id])
     end
-    like = Like.find_by(likeable: target, user: current_user)
+    like = Like.find_by(likeable: @target, user: current_user)
     if like == nil
-      Like.create(likeable: target, user: current_user)
+      Like.create(likeable: @target, user: current_user)
       @current_user_like_status = "liked"
     else
       like.destroy
       @current_user_like_status = "unliked"
     end
-    @likes_count = target.likes.count
     respond_to do |format|
-      format.html { redirect_to target }
+      format.html { redirect_to @target }
       format.js { render "shared/like_update" }
     end
   end
