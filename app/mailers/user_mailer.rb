76efe15c06bccf4eb497555428885f49e5bae5a6
KevinPiriou <kevinpiriou@yahoo.fr>
2021-12-18 20:04:10 +0100

    change link for login

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,19 +3,19 @@ class UserMailer < ApplicationMailer
  
   def welcome_email(user)
     @user = user 
-    @url  = 'https://matchandfeathp.herokuapp.com/login' 
+    @url  = 'https://matchandfeathp.herokuapp.com/Mon_Profil/sign_in' 
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
 
   def create_project_email(project)
     @project = project
-    @url = 'https://matchandfeathp.herokuapp.com/login'
+    @url = 'https://matchandfeathp.herokuapp.com/Mon_Profil/sign_in'
     mail(to: @project.user.email, subject: 'Création du projet')
   end
 
   def new_feat_email(feat)
     @feat = feat
-    @url = 'https://matchandfeathp.herokuapp.com/login'
+    @url = 'https://matchandfeathp.herokuapp.com/Mon_Profil/sign_in'
     mail(to: @feat.user.email, subject: 'Nouvelle collaboration')
   end
 end
