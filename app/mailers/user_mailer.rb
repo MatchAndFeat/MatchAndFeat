0b258d77e1f8915f7bfe1ca28e8a648a4963c0c9
fleopaulD <flo_thp@netc.pl>
2021-12-18 23:29:18 +0100

    fixed urls in mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,19 +3,19 @@ class UserMailer < ApplicationMailer
  
   def welcome_email(user)
     @user = user 
-    @url  = 'https://matchandfeathp.herokuapp.com/' 
+    @url  = "https://matchandfeathp.herokuapp.com/Profil/#{@user.id}" 
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
 
   def create_project_email(project)
     @project = project
-    @url = "https://matchandfeathp.herokuapp.com/Projet/#{project.id}"
+    @url = "https://matchandfeathp.herokuapp.com/Projets/#{project.id}"
     mail(to: @project.user.email, subject: 'Création du projet')
   end
 
   def new_feat_email(feat)
     @feat = feat
-    @url = "https://matchandfeathp.herokuapp.com/Projet/#{@feat.project.id}"
+    @url = "https://matchandfeathp.herokuapp.com/Projets/#{@feat.project.id}"
     mail(to: @feat.user.email, subject: 'Nouvelle collaboration')
   end
 end
