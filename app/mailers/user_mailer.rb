9331a3eec38f5d2e69012d0814f3572a65344f46
fleopaulD <flo_thp@netc.pl>
2021-12-20 10:43:11 +0100

    add default host for development and production env)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,21 +1,22 @@
 class UserMailer < ApplicationMailer
-  default from: 'matchandfeathp@gmail.com'
+  include Rails.application.routes.url_helpers
+  default from: ENV['EMAIL_FROM']
  
   def welcome_email(user)
-    @user = user 
-    @url  = "https://matchandfeathp.herokuapp.com/Profil/#{@user.id}" 
+    @user = user
+    @url  = url_for(@user)
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
 
   def create_project_email(project)
     @project = project
-    @url = "https://matchandfeathp.herokuapp.com/Projets/#{project.id}"
-    mail(to: @project.user.email, subject: 'Création du projet')
+    @url = url_for(@project)
+    mail(to: @project.user.email, subject: "Création du projet '#{@project.title}'")
   end
 
   def new_feat_email(feat)
     @feat = feat
-    @url = "https://matchandfeathp.herokuapp.com/Projets/#{@feat.project.id}"
-    mail(to: @feat.user.email, subject: 'Nouvelle collaboration')
+    @url = url_for(@feat.project)
+    mail(to: @feat.user.email, subject: "Nouvelle collaboration sur le projet '#{@feat.project.title}'")
   end
 end
